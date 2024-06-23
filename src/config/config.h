5637f269281c7eb4530d8bbcb83eeb9400fb9a71
Jason Hilder <jhilder95@gmail.com>
2024-06-23 16:54:25 +0200

    Config placeholders.

---
+++ src/config/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Function declarations related to configuration
+void initialize_config(void);
+int read_config(const char *filename);
+int write_config(const char *filename);
+
+#endif // CONFIG_H
+
+     
